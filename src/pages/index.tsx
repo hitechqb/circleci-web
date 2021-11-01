a37103cdfa24521fb4b683b2e4116afc17168285
Luu Duc Hoang <hitech.qb@gmail.com>
2021-11-01 08:22:55 +0700

    test deploy site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          NextJs Site
+          NextJs VietNam
         </h1>
       </main>
 
